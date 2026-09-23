9e224081900cce61c3d96af28d610b1e50d963e6
ylak <ylak90@hotmail.com>
2026-09-23 18:56:11 +0200

    part 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -6,19 +6,23 @@ const Header = (props) => {
   </h1>
 )
 }
+const Part = (props) =>{
+  return(
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
 const Content = (props) => {
   return(
-  <>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </>
+  <div>
+    <Part name={props.part1} exercises={props.exercises1}/>
+
+    <Part name={props.part2} exercises={props.exercises2}/>
+
+    <Part name={props.part3} exercises={props.exercises3}/>
+  </div>
 )
 }
 const Total = (props) => {
