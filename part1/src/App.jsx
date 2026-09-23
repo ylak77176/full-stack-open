dee155075fbadb135b3d4631cdd0f85ded7c8ab3
ylak <ylak90@hotmail.com>
2026-09-23 18:35:43 +0200

    part 1.1

---
+++ part1/src/App.jsx
@@ -0,0 +1,50 @@
+
+const Header = (props) => {
+  return(
+  <h1>
+    {props.course}
+  </h1>
+)
+}
+const Content = (props) => {
+  return(
+  <>
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  </>
+)
+}
+const Total = (props) => {
+  return(
+    <p>
+      Number of exercises {props.ex1 + props.ex2 + props.ex3}
+    </p>
+)
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1 = {part1} exercices1 = {exercises1}  part2 = {part2} exercices2 = {exercises2}  part3 = {part3} exercices3 = {exercises3} />
+      <Total ex1 = {exercises1} ex2 = {exercises2}  ex3 = {exercises3}/>
+    </div>
+  )
+}
+
+export default App
