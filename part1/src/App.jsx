57f90aa6a71fed6ab601fdb26a424377c4ae6b93
ylak <ylak90@hotmail.com>
2026-09-24 15:13:55 +0200

    1.3 refacto variable info into list

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -35,18 +35,24 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content part1 = {part1} exercices1 = {exercises1}  part2 = {part2} exercices2 = {exercises2}  part3 = {part3} exercices3 = {exercises3} />
-      <Total ex1 = {exercises1} ex2 = {exercises2}  ex3 = {exercises3}/>
+      <Content part1 = {part1.name} exercices1 = {part1.exercises}  part2 = {part2.name} exercices2 = {part2.exercises}  part3 = {part3.name} exercices3 = {part3.exercises} />
+      <Total ex1 = {part1.exercises} ex2 = {part2.exercises}  ex3 = {part3.exercises}/>
     </div>
   )
 }
